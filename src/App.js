f28f1cf4aec00911e44e0e11aa6eda14eee6717e
Hamza Shafi <hamzashafi946@gmail.com>
2023-12-04 14:43:07 +0500

    Initial lines commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//CSSBaseLine this gonna reset the css to default 
+
+import { ColorModeContext,useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
